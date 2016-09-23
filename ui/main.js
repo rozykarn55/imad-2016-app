887d0d59ce7dec5e07959ec13d1b55dda6c4ff07
rozykarn55 <rozykarn55@gmail.com>
2016-09-23 11:35:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,17 @@ element.innerhtml='This is the code done in inspect';
 
 //move the image
 var img=document.getElementById('roza');
+var marginleft=0;
 
+function moveright(){
+    marginleft=marginleft +10;
+    
+    img.style.marginleft + 'px';
+    
+    
+}
 img.onClick = function (){
+    var interval=setInterval(moveright,100);
     
     img.style.Marginleft='100px';
     

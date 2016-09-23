ecabd580975c6f8f89874f9072ea8cccf1584c9c
rozykarn55 <rozykarn55@gmail.com>
2016-09-23 11:29:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,10 @@ element.innerhtml='This is the code done in inspect';
 
 //move the image
 var img=document.getElementById('roza');
+
 img.onClick = function (){
+    
     img.style.Marginleft='100px';
+    
     
 };

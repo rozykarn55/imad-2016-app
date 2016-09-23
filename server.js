ce115e11c1fc7b1e73583d62de0cc35ea85b8201
rozykarn55 <rozykarn55@gmail.com>
2016-09-23 10:05:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mypage1',function(req,res){
-    res.send('This is my first notice to this application');
+   res.sendFile(path.join(__dirname, 'ui', 'roz.html'));
     
 });
 app.get('/mypage2',function(req,res){
